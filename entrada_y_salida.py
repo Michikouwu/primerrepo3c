22f8fcc5c4927657e7fbfca52e06af3d9680eeaf
PAULA VALERIA VALERIA <90258573+Michikouwu@users.noreply.github.com>
2021-09-20 17:30:17 +0000

    Creamos el archivo entrada_y_salida.py

---
+++ entrada_y_salida.py
@@ -0,0 +1,14 @@
+# Este programa muestra la forma de recoger informacion desde el teclado
+# 20 de Septiembre 
+
+print("Hola ! como te llamas?")
+miNombre = input() # input() recibe texto desde el teclado
+print("Hola", miNombre, " espero que lo estes pasando genial")
+print("Que edad tienes?")
+
+
+if edad > 13:
+  print("Ok puedes entrar")
+else:
+ print("mmm no, aqui no se permiten niños")
+
