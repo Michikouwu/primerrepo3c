1bfac94e07a0d63d41276cf9b05ffb910ff1a6ce
PAULA VALERIA VALERIA <90258573+Michikouwu@users.noreply.github.com>
2021-10-12 12:42:36 +0000

    cuarto_rectangular

---
+++ cuarto_rectangular.py
@@ -0,0 +1,3 @@
+# Este programa calcula el area de un cuarto rectangular y muestra el resultado en el sistema metrico (metrosccuadrados) y en el sistema imperial (pies cuadrados) 
+# 1. Inicio 
+# 2. Definir ancho, largo 
